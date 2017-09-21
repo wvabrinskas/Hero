890a2c81667d15d3686d8c04b3f94040306ffcd6
Wiliam Vabrinskas <wvabrinskas@gmail.com>
2017-09-21 09:50:53 -0400

    fixed for swift 4

--- Sources/Animator/HeroCoreAnimationViewContext.swift
+++ Sources/Animator/HeroCoreAnimationViewContext.swift
@@ -26,7 +26,7 @@ extension CALayer {
   internal static var heroAddedAnimations: [(CALayer, String, CAAnimation)]? = {
     let swizzling: (AnyClass, Selector, Selector) -> Void = { forClass, originalSelector, swizzledSelector in
       if let originalMethod = class_getInstanceMethod(forClass, originalSelector), let swizzledMethod = class_getInstanceMethod(forClass, swizzledSelector) {
-        method_exchangeImplementations(originalMethod, swizzledMethod)
+        method_exchangeImplementations(originalMethod!, swizzledMethod)
       }
     }
     let originalSelector = #selector(add(_:forKey:))
